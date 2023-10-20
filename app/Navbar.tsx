1a93c7d82ad5eefab97e334a6b4dcbab0bac7823
muloo <fly20091124@126.com>
2023-10-20 15:40:42 +0800

    add navbar style

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,12 @@
+"use client"
 import Link from 'next/link'
 import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
+import { usePathname } from 'next/navigation'
+import cls from 'classnames'
 
 const Navbar = () => {
+  const pathname = usePathname()
   const links = [{
     label: 'Dashboard', link: '/'
   }, {
@@ -12,7 +16,16 @@ const Navbar = () => {
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
       <Link href="/"><AiFillBug /></Link>
       <ul className='flex space-x-6'>
-        {links.map(item => <li key={item.link}><Link href={item.link} className='text-zinc-500 hover:text-zinc-800 transition-colors'>{item.label}</Link></li>)}
+        {links.map(item =>
+          <li key={item.link}>
+            <Link
+              href={item.link}
+              className={cls('text-zinc-500 hover:text-zinc-800 transition-colors', {
+                'text-blue-500': pathname === item.link
+              })}>
+              {item.label}
+            </Link>
+          </li>)}
       </ul>
     </nav>
   )
