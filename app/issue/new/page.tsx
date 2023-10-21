6b5b8aceeb9111d85c2303c35a71601fce8e9217
muloo <fly20091124@126.com>
2023-10-21 22:57:50 +0800

    Add client-side validation

--- app/issue/new/page.tsx
+++ app/issue/new/page.tsx
@@ -1,19 +1,24 @@
 "use client"
 import React, { useState } from 'react'
-import { Button, TextArea, TextField, Callout } from '@radix-ui/themes'
+import { Button, TextArea, TextField, Callout, Text } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from 'react-hook-form';
 import axios from "axios";
 import { useRouter } from 'next/navigation';
 import { InfoCircledIcon } from '@radix-ui/react-icons';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchema';
+import { z } from 'zod';
 
-interface IssuesProps {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>; // 根据schema生成表单类型
+
+
 const NewIssuePage = () => {
-  const { register, handleSubmit, control } = useForm();
+  const { register, handleSubmit, control, formState: { errors } } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema)
+  });
+  console.log(errors)
   const [error, setError] = useState<string>();
   const router = useRouter();
   return (
@@ -22,19 +27,21 @@ const NewIssuePage = () => {
         try {
           await axios.post('/api/issues', fileds)
           router.push('/issue')
-        } catch (error) {
+        } catch (error: any) {
           console.log(error)
-          setError(error?.message || 'Something went wrong');
+          setError(error?.response?.data?.message || 'Something went wrong');
         }
       })}>
         <TextField.Root >
           <TextField.Input placeholder="Add issues ..." {...register('title')} />
         </TextField.Root>
+        {errors.title && <Text color='red' as='p'>{errors.title.message}</Text>}
         <Controller
           control={control}
           name="description"
           render={({ field }) => <SimpleMDE spellCheck={false} placeholder='add description ...' {...field} />}
         />
+          {errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
         <Button>New Issue</Button>
       </form>
       {error &&
