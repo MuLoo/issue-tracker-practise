d36466124f24e5e3b2c4a571afe727f6605a6e24
muloo <fly20091124@126.com>
2023-10-21 12:40:52 +0800

    Handle form submisstion

--- app/issue/new/page.tsx
+++ app/issue/new/page.tsx
@@ -1,16 +1,36 @@
 "use client"
 import React from 'react'
 import { Button, TextArea, TextField } from '@radix-ui/themes'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from 'react-hook-form';
+import axios from "axios";
+import { useRouter } from 'next/navigation';
 
+interface IssuesProps {
+  title: string;
+  description: string;
+}
 const NewIssuePage = () => {
+  const { register, handleSubmit, control } = useForm();
+  const router = useRouter();
   return (
-    <div className='space-y-4 max-w-lg'>
+    <form className='space-y-4 max-w-xl' onSubmit={handleSubmit(async (fileds) => {
+      console.log(fileds)
+      await axios.post('/api/issues', fileds)
+      router.push('/issue')
+    })}>
       <TextField.Root >
-        <TextField.Input placeholder="Add issues ..." />
+        <TextField.Input placeholder="Add issues ..." {...register('title')} />
       </TextField.Root>
-      <TextArea placeholder='add description ...'/>
+      <Controller
+        control={control}
+        name="description"
+        render={({ field }) => <SimpleMDE placeholder='add description ...' {...field} />}
+      />
+     
       <Button>New Issue</Button>
-    </div>
+    </form>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme appearance="light" radius="large">
+        <Theme radius="large">
           <Navbar />
           <main className='p-5'>
             {children}
