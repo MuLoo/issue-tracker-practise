e124dceefe3775ce34cd779b095622eb0689d4e4
muloo <fly20091124@126.com>
2023-11-28 17:04:43 +0800

    feat: add dropmenu to show user

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,9 +4,12 @@ import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
 import { usePathname } from 'next/navigation'
 import cls from 'classnames'
+import { useSession } from 'next-auth/react'
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes'
 
 const Navbar = () => {
 	const pathname = usePathname()
+	const { status, data: session } = useSession()
 	const links = [
 		{
 			label: 'Dashboard',
@@ -18,23 +21,53 @@ const Navbar = () => {
 		}
 	]
 	return (
-		<nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-			<Link href="/">
-				<AiFillBug />
-			</Link>
-			<ul className="flex space-x-6">
-				{links.map(item => (
-					<li key={item.link}>
-						<Link
-							href={item.link}
-							className={cls('text-zinc-500 hover:text-zinc-800 transition-colors', {
-								'text-blue-500': pathname === item.link
-							})}>
-							{item.label}
+		<nav className="border-b mb-5 px-5 h-14 py-3">
+			<Container>
+				<Flex justify="between">
+					<Flex align="center" gap="3">
+						<Link href="/">
+							<AiFillBug />
 						</Link>
-					</li>
-				))}
-			</ul>
+						<ul className="flex space-x-6">
+							{links.map(item => (
+								<li key={item.link}>
+									<Link
+										href={item.link}
+										className={cls('text-zinc-500 hover:text-zinc-800 transition-colors', {
+											'text-blue-500': pathname === item.link
+										})}>
+										{item.label}
+									</Link>
+								</li>
+							))}
+						</ul>
+					</Flex>
+					<Box>
+						{status === 'authenticated' && (
+							<DropdownMenu.Root>
+								<DropdownMenu.Trigger>
+									<Avatar
+										src={session?.user?.image ?? ''}
+										fallback="?"
+										size="2"
+										radius="full"
+										className="cursor-pointer"
+									/>
+								</DropdownMenu.Trigger>
+								<DropdownMenu.Content align="end">
+									<DropdownMenu.Label>
+										<Text size="2">{session?.user?.email}</Text>
+									</DropdownMenu.Label>
+									<DropdownMenu.Item>
+										<Link href="/api/auth/signout">Sign Out</Link>
+									</DropdownMenu.Item>
+								</DropdownMenu.Content>
+							</DropdownMenu.Root>
+						)}
+						{status === 'unauthenticated' && <Link href="/api/auth/signin">Sign In</Link>}
+					</Box>
+				</Flex>
+			</Container>
 		</nav>
 	)
 }
--- app/issue/[id]/IssueDetail.tsx
+++ app/issue/[id]/IssueDetail.tsx
@@ -5,23 +5,19 @@ import ReactMarkdown from 'react-markdown'
 import { Issues } from '@prisma/client' // 从prisma里获取到类型
 
 const IssueDetail = (props: Issues) => {
-  const {
-    status,
-    createdAt,
-    description
-  } = props;
-  return (
-    <>
-      <Heading>Issue Detail</Heading>
-        <Flex className='space-x-3' my="2">
-          <IssueStatusBadge status={status} />
-          <p>{createdAt.toDateString()}</p>
-        </Flex>
-        <Card className='prose max-w-full' mt="4">
-          <ReactMarkdown>{description}</ReactMarkdown>
-          </Card>
-    </>
-  )
+	const { status, createdAt, description } = props
+	return (
+		<>
+			<Heading>Issue Detail</Heading>
+			<Flex className="space-x-3" my="2">
+				<IssueStatusBadge status={status} />
+				<p>{createdAt.toDateString()}</p>
+			</Flex>
+			<Card className="prose max-w-full" mt="4">
+				<ReactMarkdown>{description}</ReactMarkdown>
+			</Card>
+		</>
+	)
 }
 
 export default IssueDetail
